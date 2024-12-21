cffe7431df5e49cbf53fda3543372aefad599db5
MirrorInstinct <jarolvaz97@gmail.com>
2024-12-21 12:45:22 -0500

    created more elements and added css

--- script.js
+++ script.js
@@ -6,12 +6,51 @@ fetch('./data.json')
         const jobList = document.getElementById('job-list');
 
         data.forEach(job => {
+
+            //create divs with a class name
+            const jobDiv = document.createElement('div');
+            const jobInfo = document.createElement('div');
+            const jobNotes = document.createElement('div');
+
+            jobDiv.className = 'job-div';
+            jobInfo.className = 'job-info';
+            jobNotes.className = 'job-notes';
             
-            const jobItem = document.createElement('p');
+            //create new elements for each specified entry
+            const jobIcon = document.createElement('img');
+            const jobCompany = document.createElement('h3');
+            const jobPosition = document.createElement('p');
+            const jobNew = document.createElement('h4');
+            const jobPosted = document.createElement('p');
+            const jobContract = document.createElement('p');
+            const jobLocation = document.createElement('p');
 
-            jobItem.textContent = `${job.company} ${job.position}`;
+            //fill in each element with json data
+            jobIcon.src = `${job.logo}`;
+            jobCompany.textContent = `${job.company}`;
+            jobNew.textContent=`${job.new}`;
+            jobPosition.textContent = `${job.position}`;
+            jobPosted.textContent = `${job.postedAt}`;
+            jobContract.textContent = `${job.contract}`;
+            jobLocation.textContent = `${job.location}`;
 
-            jobList.appendChild(jobItem);
+            //append each entry
+            jobDiv.appendChild(jobIcon);
+
+            jobInfo.appendChild(jobCompany);
+            jobInfo.appendChild(jobNew);
+            jobInfo.appendChild(jobPosition);
+
+            jobNotes.appendChild(jobPosted);
+            jobNotes.appendChild(jobContract);
+            jobNotes.appendChild(jobLocation);
+
+            //append into another div
+            jobDiv.appendChild(jobInfo);
+            jobInfo.appendChild(jobNotes);
+
+            //append the new divs into this main div
+            jobList.appendChild(jobDiv);
 
             // //create new items
             // const jobIcon = document.createElement('img');
